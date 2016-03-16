5af134c678f8a0d2123bc948c2345cb11948bb20
nepby <nepby@outlook.com>
2016-03-16 10:24:37 +0300

    Update init.sh

--- etc/init.sh
+++ etc/init.sh
@@ -4,5 +4,5 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 cd /home/box/web/
-sudo gunicorn -c /home/box/web/etc/gunicorn.py hello:app
+sudo gunicorn -c /home/box/web/etc/hello.py hello:app
 
